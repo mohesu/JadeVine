3edcae3a9fe7d97f4d31c4b28fd5fc219498b72f
Rishit-Thaper <rishitthaper5329@gmail.com>
2023-10-22 16:54:29 +0530

    First Commit

---
+++ JS/gallery.js
@@ -0,0 +1,117 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector("header");
+    // const header = document.querySelector(".home");
+    const gallery = document.querySelector(".gallery-home")
+    // const headerHeight = header.offsetHeight;
+    const galleryHeight = gallery.offsetHeight;
+    let isNavbarTransparent = true;
+  
+    function updateNavbarBackground() {
+      const scrollY = window.scrollY;
+      const shouldNavbarBeTransparent =  scrollY < galleryHeight;
+  
+      if (shouldNavbarBeTransparent !== isNavbarTransparent) {
+        isNavbarTransparent = shouldNavbarBeTransparent;
+        requestAnimationFrame(() => {
+          navbar.style.transition = "background-color 0.3s ease"; 
+          navbar.style.backgroundColor = isNavbarTransparent
+            ? "transparent"
+            : "#44261e"; 
+        });
+      }
+    }
+    
+  
+  function debounce(func, wait) {
+  let timeout;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+  }
+  
+  const debouncedUpdateNavbarBackground = debounce(updateNavbarBackground, 10);
+    window.addEventListener("scroll", debouncedUpdateNavbarBackground);
+  });
+
+//Function to make Navbar Responsive
+document.addEventListener("click", function (event) {
+  var navMenu = document.querySelector(".nav-menu");
+  var toggleButton = document.querySelector(".toggle-button");
+
+  var isToggleButton = event.target === toggleButton;
+  var isMenuActive = navMenu.classList.contains("active");
+
+  if (isToggleButton || (isMenuActive && !navMenu.contains(event.target))) {
+    if (isMenuActive) {
+      closeNavMenu();
+    } else {
+      openNavMenu();
+    }
+  }
+});
+
+// Function to close the navigation menu
+function closeNavMenu() {
+  var navMenu = document.querySelector(".nav-menu");
+  navMenu.classList.remove("active");
+  navMenu.style.transform = "translateY(-200px)";
+}
+
+// Function to open the navigation menu
+function openNavMenu() {
+  var navMenu = document.querySelector(".nav-menu");
+  navMenu.classList.add("active");
+  navMenu.style.transform = "translateY(0)";
+}
+
+window.addEventListener("scroll", function () {
+  var isMenuActive = document.querySelector(".nav-menu").classList.contains("active");
+  
+  if (isMenuActive) {
+    closeNavMenu();
+  }
+});
+
+
+  function animateOnScrollRight(elements) {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate__animated', 'animate__fadeInRight');
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+  
+    elements.forEach((item) => {
+      observer.observe(item);
+    });
+}
+function animateOnScrollLeft(elements) {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate__animated', 'animate__fadeInLeft');
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+  
+    elements.forEach((item) => {
+      observer.observe(item);
+    });
+}
+
+const rightOne = document.querySelectorAll('.right-one');
+const leftOne = document.querySelectorAll('.left-one');
+const rightTwo = document.querySelectorAll('.right-two');
+const leftTwo = document.querySelectorAll('.left-two');
+
+animateOnScrollLeft(leftOne);
+animateOnScrollRight(rightOne);
+
+animateOnScrollLeft(leftTwo);
+animateOnScrollRight(rightTwo);
---
+++ JS/main.js
@@ -0,0 +1,131 @@
+//function to add Background Color to the Navbar
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector("header");
+    const header = document.querySelector(".home");
+    const headerHeight = header.offsetHeight;
+    let isNavbarTransparent = true;
+  
+    function updateNavbarBackground() {
+      const scrollY = window.scrollY;
+      const shouldNavbarBeTransparent = scrollY < headerHeight;
+  
+      if (shouldNavbarBeTransparent !== isNavbarTransparent) {
+        isNavbarTransparent = shouldNavbarBeTransparent;
+        requestAnimationFrame(() => {
+          navbar.style.transition = "background-color 0.3s ease"; 
+          navbar.style.backgroundColor = isNavbarTransparent
+            ? "transparent"
+            : "#44261e"; 
+        });
+      }
+    }
+    
+
+function debounce(func, wait) {
+  let timeout;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
+
+const debouncedUpdateNavbarBackground = debounce(updateNavbarBackground, 10);
+    window.addEventListener("scroll", debouncedUpdateNavbarBackground);
+});
+
+
+
+  
+//Function to make Navbar Responsive
+document.addEventListener("click", function (event) {
+  var navMenu = document.querySelector(".nav-menu");
+  var toggleButton = document.querySelector(".toggle-button");
+
+  var isToggleButton = event.target === toggleButton;
+  var isMenuActive = navMenu.classList.contains("active");
+
+  if (isToggleButton || (isMenuActive && !navMenu.contains(event.target))) {
+    if (isMenuActive) {
+      closeNavMenu();
+    } else {
+      openNavMenu();
+    }
+  }
+});
+
+// Function to close the navigation menu
+function closeNavMenu() {
+  var navMenu = document.querySelector(".nav-menu");
+  navMenu.classList.remove("active");
+  navMenu.style.transform = "translateY(-200px)";
+}
+
+// Function to open the navigation menu
+function openNavMenu() {
+  var navMenu = document.querySelector(".nav-menu");
+  navMenu.classList.add("active");
+  navMenu.style.transform = "translateY(0)";
+}
+
+window.addEventListener("scroll", function () {
+  var isMenuActive = document.querySelector(".nav-menu").classList.contains("active");
+  
+  if (isMenuActive) {
+    closeNavMenu();
+  }
+});
+
+//Function to make Image Slideshow
+
+const imageContainer = document.querySelector('.image-container');
+const imageUrls = ["Rest/room6.jpg", "Rest/building.jpg", "Rest/room9.jpg", "Rest/room4.jpg", "Rest/room10.jpg"];
+let currentImageIndex = 0;
+
+function updateImage() {
+  imageContainer.style.backgroundImage = `url(${imageUrls[currentImageIndex]})`;
+
+  currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
+}
+
+setInterval(updateImage, 3000); 
+updateImage();
+
+
+
+//Function to make Animation on Scroll
+function animateOnScroll(elements) {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate__animated', 'animate__fadeInUp');
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+  
+    elements.forEach((item) => {
+      observer.observe(item);
+    });
+}
+
+// const aboutSection = document.querySelectorAll('.about-section');
+const roomSection = document.querySelectorAll('.room-cards');
+const menuSection = document.querySelectorAll('.menu-grid');
+const interiorSection = document.querySelectorAll('.interior-grid');
+const dishesSection = document.querySelectorAll('.drink-dishes');
+
+// animateOnScroll(aboutSection);
+animateOnScroll(roomSection);
+animateOnScroll(interiorSection);
+animateOnScroll(menuSection);
+animateOnScroll(dishesSection);
+
+
+
+
+
+
+
